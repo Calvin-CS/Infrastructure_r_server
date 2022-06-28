5beb3fe117407e4c84859221d27ffe1e195cc7c9
cwieri39 <cwieri39@theia.cs.calvin.edu>
2022-06-28 15:24:36 -0400

    fixed ENV variable typo

--- install-Rstudio.sh
+++ install-Rstudio.sh
@@ -3,7 +3,7 @@
 # install Rstudio
 export RSW_VERSION=2022.02.2+485.pro2
 export RSW_NAME=rstudio-workbench
-export RWS_DOWNLOAD_URL=https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64
+export RSW_DOWNLOAD_URL=https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64
 
 apt update --fix-missing
 export RWS_VERSION_URL=`echo -n "${RSW_VERSION}" | sed 's/+/-/g'`
