186117663b665bb1819e13603651005ab16cb77b
cwieri39 <cwieri39@theia.cs.calvin.edu>
2022-06-28 14:24:23 -0400

    updated anaconda list

--- install-anaconda.sh
+++ install-anaconda.sh
@@ -6,13 +6,12 @@ export PYTHON_VERSION=3.9.12
 
 curl -o anaconda.sh $ANACONDA_DOWNLOAD_URL/Anaconda3-$ANACONDA_VER-Linux-x86_64.sh 
 bash anaconda.sh -b -p /opt/anaconda -u
-/opt/anaconda/bin/conda install -y python==$PYTHON_VERSION
 rm -rf anaconda.sh
 
 cd /opt/anaconda
 export PATH=/opt/anaconda/bin:$PATH
 conda install mamba -c conda-forge -y
-mamba install -c fastai -c pytorch -c conda-forge anaconda fastai jupyter fastbook voila jedi pytorch torchvision torchaudio cudatoolkit folium plotly scikit-learn matplotlib pandas anaconda-navigator jupyterlab tensorflow-gpu -y
+mamba install -c fastai -c pytorch -c conda-forge anaconda fastai jupyter fastbook voila jedi pytorch torchvision torchaudio cudatoolkit folium plotly scikit-learn matplotlib pandas anaconda-navigator jupyterlab tensorflow-cpu tensorflow-gpu -y
 pip install gmplot sentencepiece -y
 pip install numpy guizero gpiozero requests pillow scipy matplotlib pgzero pygame flask pytest colorama birdseye beautifulsoup4 pandas lxml openpyxl XlsxWriter xlrd xlwt html5lib odfpy -y
 pip install 'fastcore>1.3.26' -y
