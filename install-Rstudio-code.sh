0aec12c4d5b0564efa602dcf6645932041560cbe
root <root@r.cs.calvin.edu>
2022-06-29 18:56:04 +0000

    add vscode settings

--- install-Rstudio-code.sh
+++ install-Rstudio-code.sh
@@ -2,3 +2,7 @@
 
 # install VS Code server using rstudio-server installers
 rstudio-server install-vs-code /opt/code-server
+
+# copy conf files
+cp -f vscode.conf /etc/rstudio/
+cp -f vscode-user-settings.json /etc/rstudio/
