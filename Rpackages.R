278f37acef5c0452c958cc8ce499a69f0a702ba6
root <root@r.cs.calvin.edu>
2022-08-17 18:57:56 +0000

    add gapminder

--- Rpackages.R
+++ Rpackages.R
@@ -1,5 +1,5 @@
 # new smart install way
-my_packages <- c('mosaic','tidyverse','reticulate','devtools','keras','nycflights13','pins','plotly','qualtRics','skimr','tidymodels','timeDate','mdsr','fpp3','sf','terra','spData','tmap','rcartocolor','patchwork','AmesHousing','rpart.plot')
+my_packages <- c('mosaic','tidyverse','reticulate','devtools','keras','nycflights13','pins','plotly','qualtRics','skimr','tidymodels','timeDate','mdsr','fpp3','sf','terra','spData','tmap','rcartocolor','patchwork','AmesHousing','rpart.plot','gapminder')
 not_installed <- my_packages[!(my_packages %in% installed.packages()[ , "Package"])]
 if(length(not_installed)) install.packages(not_installed,repos = "http://cran.us.r-project.org")
 print(paste(length(not_installed), "packages had to be installed."))
