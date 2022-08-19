0766da7c128e6df9913a73aa076b16e4ece83d08
Chris Wieringa <cwieri39@calvin.edu>
2022-08-19 15:56:30 +0000

    update rpackages

--- Rpackages.R
+++ Rpackages.R
@@ -1,5 +1,5 @@
 # new smart install way
-my_packages <- c('mosaic','tidyverse','reticulate','devtools','keras','nycflights13','pins','plotly','qualtRics','skimr','tidymodels','timeDate','mdsr','fpp3','sf','terra','spData','tmap','rcartocolor','patchwork','AmesHousing','rpart.plot','gapminder','palmerpenguins','Lock5withR')
+my_packages <- c('mosaic','tidyverse','reticulate','devtools','keras','nycflights13','pins','plotly','qualtRics','skimr','tidymodels','timeDate','mdsr','fpp3','sf','terra','spData','tmap','rcartocolor','patchwork','AmesHousing','rpart.plot','gapminder','palmerpenguins','Lock5withR','abd','bookdown','bootstrap','car','carData','DAAG','Devore7','faraway','fastR2','GGally','Hmisc','ISLR','janitor','Lahman','naniar','NHANES','nhanesA','openintro','partykit','shinyjs','shinystan','shinytest','shinythemes','TeachingDemos','xgboost')
 not_installed <- my_packages[!(my_packages %in% installed.packages()[ , "Package"])]
 if(length(not_installed)) install.packages(not_installed,repos = "http://cran.us.r-project.org")
 print(paste(length(not_installed), "packages had to be installed."))
@@ -8,9 +8,11 @@ print(paste(length(not_installed), "packages had to be installed."))
 install_github_if_missing <- function(user, pkg) {
   if (!nzchar(system.file(package = pkg))) {
     remotes::install_github(paste0(user, "/", pkg))
+    print("1 package had to be installed.")
   }
 }
 
 install_github_if_missing("rstudio","learnr")
 install_github_if_missing("rstudio-education","dsbox")
 install_github_if_missing("geocompr","geocompkg")
+install_github_if_missing("dtkaplan","submitr")
--- update-install-Rpackage.sh
+++ update-install-Rpackage.sh
@@ -5,6 +5,6 @@ for dir in /opt/R/*; do
   if [[ $Rver != 'lost+found' ]]; then
     echo "Updating $Rver"
     export PATH=$ORIGPATH:/opt/R/${Rver}/bin
-    Rscript Rpackages.R
+    /opt/R/${Rver}/bin/Rscript Rpackages.R
   fi
 done
