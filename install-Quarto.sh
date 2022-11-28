5d695c11f1129d8fec668d8ddab2584ec75fb01e
Chris Wieringa <cwieri39@calvin.edu>
2022-11-28 17:06:16 +0000

    update quarto get string

--- install-Quarto.sh
+++ install-Quarto.sh
@@ -2,7 +2,7 @@
 
 # install Quarto 
 export QUARTO_VERSION="1.2.269"
-curl -O https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
 gdebi -n quarto-${QUARTO_VERSION}-linux-amd64.deb
 rm -f quarto-${QUARTO_VERSION}-linux-amd64.deb
 
