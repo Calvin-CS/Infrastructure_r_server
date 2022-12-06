be944f2ec2b7c91d83566769689e272cb97143a6
root <root@connect.cs.calvin.edu>
2022-12-06 16:01:33 +0000

    add Connect install script

---
+++ install-Connect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install R package dependencies
+xargs apt install -y < Rpackages.dep
+
+# install R
+export CONNECT_VERSION=2022.11
+export CONNECT_SUBVERSION=0
+curl -O https://cdn.rstudio.com/connect/${CONNECT_VERSION}/rstudio-connect_${CONNECT_VERSION}.${CONNECT_SUBVERSION}~ubuntu20_amd64.deb
+gdebi -n rstudio-connect_${CONNECT_VERSION}.${CONNECT_SUBVERSION}~ubuntu20_amd64.deb
+rm -rf rstudio-connect_${CONNECT_VERSION}.${CONNECT_SUBVERSION}~ubuntu20_amd64.deb
