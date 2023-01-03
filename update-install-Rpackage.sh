5cda219274394d90a196d1cc396b02fa117702c3
root <root@r.cs.calvin.edu>
2023-01-03 21:21:10 +0000

    skip self-referencial R directory

--- update-install-Rpackage.sh
+++ update-install-Rpackage.sh
@@ -2,7 +2,9 @@
 ORIGPATH=$PATH
 for dir in /opt/R/*; do
   Rver="$(basename -- $dir)"
-  if [[ $Rver != 'lost+found' ]]; then
+  if [[ $Rver == 'lost+found' || $Rver == 'R' ]]; then
+    echo "Skipping directory named $Rver"
+  else
     echo "Updating $Rver"
     export PATH=$ORIGPATH:/opt/R/${Rver}/bin
     /opt/R/${Rver}/bin/Rscript Rpackages.R
