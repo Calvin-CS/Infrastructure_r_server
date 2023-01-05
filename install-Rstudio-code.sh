f69d730be5abe5d8d4f312fa79c2d18a39f106ad
root <root@r.cs.calvin.edu>
2023-01-05 15:34:23 +0000

    updated code install

--- install-Rstudio-code.sh
+++ install-Rstudio-code.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # install VS Code server using rstudio-server installers
-rstudio-server install-vs-code /opt/code-server
+# note that they removed the dedicated command to do this, so I have to rsync
+# it from where it is installed to the NFS mount in /opt/code-server
+#rstudio-server install-vs-code /opt/code-server
+cd /usr/lib/rstudio-server/bin
+rsync -av --delete code-server /opt/
+cd /opt/code-server/
+ln -s ./bin/code-server code-server
+ln -s ./lib/node node
 
 # copy conf files
 cp -f vscode.conf /etc/rstudio/
