c5bd9fe97ba5f755cd1bd8d7906e440c212502f1
Chris Wieringa <cwieri39@calvin.edu>
2023-03-27 12:13:09 -0400

    cmdstan upgrade to git and fix dir permissions

--- install-cmdstan.sh
+++ install-cmdstan.sh
@@ -3,21 +3,28 @@
 # install cmdstan dependencies
 apt install build-essential
 
-# download cmdstan
-export CMDSTAN_VERSION=2.31.0
+# download cmdstan (via github)
+#export CMDSTAN_VERSION=2.31.0
 cd /tmp
-wget https://github.com/stan-dev/cmdstan/releases/download/v${CMDSTAN_VERSION}/cmdstan-${CMDSTAN_VERSION}.tar.gz
-tar zxfv cmdstan-${CMDSTAN_VERSION}.tar.gz
+git clone https://github.com/stan-dev/cmdstan.git --depth=1 --recursive 
+#wget https://github.com/stan-dev/cmdstan/releases/download/v${CMDSTAN_VERSION}/cmdstan-${CMDSTAN_VERSION}.tar.gz
+#tar zxfv cmdstan-${CMDSTAN_VERSION}.tar.gz
 
 # build for linux
-cd cmdstan-${CMDSTAN_VERSION}
-make build
+#cd cmdstan-${CMDSTAN_VERSION}
+cd cmdstan
+make build -j4
 
 # copy everything to /usr/local; first remove old version
 rm -rf /usr/local/cmdstan
 cd /tmp
-mv cmdstan-${CMDSTAN_VERSION} cmdstan
+#mv cmdstan-${CMDSTAN_VERSION} cmdstan
 mv cmdstan /usr/local/
+
+# fixup permissions; apparently these directories need to be writable by the user /sigh
+chmod 0777 /usr/local/cmdstan
+#find /usr/local/cmdstan -type f -exec chmod 0666 {} \;
+find /usr/local/cmdstan -type d -exec chmod 0777 {} \;
 
 # add to path
 if [ ! -f /etc/profile.d/cmdstan.sh ]
@@ -29,4 +36,4 @@ then
 fi
 
 # cleanup
-rm -f /tmp/cmdstan-${CMDSTAN_VERSION}.tar.gz
+#rm -f /tmp/cmdstan-${CMDSTAN_VERSION}.tar.gz
