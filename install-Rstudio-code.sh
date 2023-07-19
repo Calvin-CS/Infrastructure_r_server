6f83b9d57cffc64108e9cb1cd1947d2a12058d32
root <root@r.cs.calvin.edu>
2023-07-19 19:52:27 +0000

    Updated for R server paths

--- install-Rstudio-code.sh
+++ install-Rstudio-code.sh
@@ -4,9 +4,10 @@
 # note that they removed the dedicated command to do this, so I have to rsync
 # it from where it is installed to the NFS mount in /opt/code-server
 #rstudio-server install-vs-code /opt/code-server
+# Update: 2023-07-19 -- rsync to /export/code which is bind-mounted to /opt/code-server
 cd /usr/lib/rstudio-server/bin
-rsync -av --delete code-server /opt/
-cd /opt/code-server/
+rsync -av --delete code-server/* /export/code/
+cd /export/code/
 ln -s ./bin/code-server code-server
 ln -s ./lib/node node
 
