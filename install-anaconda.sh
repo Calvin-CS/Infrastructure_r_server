ed2a5f73ba03005e386cb15632ad81dd7940f32d
root <root@katz.cs.calvin.edu>
2023-07-19 14:28:39 -0400

    updates to anaconda

--- install-anaconda.sh
+++ install-anaconda.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-export ANACONDA_VER=2023.07
+export ANACONDA_VER=2023.07-0
 export ANACONDA_DOWNLOAD_URL=https://repo.anaconda.com/archive
 export ANACONDA_PATH=/export/anaconda
-#export MAMBAFORGE_DOWNLOAD_URL=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
-#export MAMBAFORGE_DOWNLOAD_URL=https://github.com/conda-forge/miniforge/releases/download/4.14.0-2/Mambaforge-4.14.0-2-Linux-x86_64.sh
-#export MAMBAFORGE_DOWNLOAD_URL=https://github.com/conda-forge/miniforge/releases/download/4.13.0-1/Mambaforge-4.13.0-1-Linux-x86_64.sh
-
-# download and install mambaforge
-#wget -O mambaforge.sh $MAMBAFORGE_DOWNLOAD_URL
-#bash mambaforge.sh -b -p $ANACONDA_PATH -u
-#rm -rf mambaforge.sh
 
 # download and install base anaconda
 wget -O anaconda.sh $ANACONDA_DOWNLOAD_URL/Anaconda3-$ANACONDA_VER-Linux-x86_64.sh 
@@ -31,12 +23,10 @@ cd $ANACONDA_PATH
 export PATH=${ANACONDA_PATH}/bin:$PATH
 echo "**** Path set"
 echo $PATH
-echo "**** Install mamba"
-conda install -y mamba -n base -c conda-forge
-#echo "Mamba - install Anaconda"
-#/usr/bin/yes | mamba install anaconda
-echo "**** Mamba - install packages"
-/usr/bin/yes | mamba install fastai \
+echo "**** Install conda-libmamba-solver"
+conda install -y conda-libmamba-solver --solver=classic
+echo "**** Conda - install packages (mambasolver)"
+conda install -y fastai \
 	fastbook \
 	voila \
 	jedi \
@@ -50,7 +40,7 @@ echo "**** Mamba - install packages"
 	matplotlib \
 	pandas \
 	anaconda-navigator \
-	tensorflow 
+	tensorflow
 echo "**** Pip - install packages"
 pip install gmplot \
 	sentencepiece
@@ -87,7 +77,7 @@ pip install jupyter \
 	rsconnect_jupyter \
 	rsconnect_python \
 	workbench_jupyterlab
-pip install ryp2
+pip install rpy2
 /usr/bin/yes | conda clean -a
 
 # enable jupyer extensions
