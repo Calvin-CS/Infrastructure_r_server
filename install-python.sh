4c77bf81694356e0a6018c4da51b9a1e06d1c2ef
root <root@r.cs.calvin.edu>
2023-07-20 13:24:36 +0000

    revert python path change; needed in rstudio-rsession container

--- install-python.sh
+++ install-python.sh
@@ -6,15 +6,15 @@ export MINICONDA_DOWNLOAD_URL=https://repo.anaconda.com/miniconda
 
 # install Python via miniconda
 curl -o miniconda.sh $MINICONDA_DOWNLOAD_URL/Miniconda3-$MINICONDA_VERSION-Linux-x86_64.sh
-bash miniconda.sh -bp /export/python/$PYTHON_VERSION
-/export/python/$PYTHON_VERSION/bin/conda install -y python==$PYTHON_VERSION
+bash miniconda.sh -bp /opt/python/$PYTHON_VERSION
+/opt/python/$PYTHON_VERSION/bin/conda install -y python==$PYTHON_VERSION
 rm -rf miniconda.sh
 
 # install and enable jupyter and appropriate notebook extensions
-export PATH=/export/python/$PYTHON_VERSION/bin:$PATH
-/export/python/$PYTHON_VERSION/bin/pip install jupyter jupyterlab rsp_jupyter rsconnect_jupyter rsconnect_python workbench_jupyterlab sklearn pandas
-/export/python/$PYTHON_VERSION/bin/jupyter-nbextension install --sys-prefix --py rsp_jupyter 
-/export/python/$PYTHON_VERSION/bin/jupyter-nbextension enable --sys-prefix --py rsp_jupyter 
-/export/python/$PYTHON_VERSION/bin/jupyter-nbextension install --sys-prefix --py rsconnect_jupyter 
-/export/python/$PYTHON_VERSION/bin/jupyter-nbextension enable --sys-prefix --py rsconnect_jupyter 
-/export/python/$PYTHON_VERSION/bin/jupyter-serverextension enable --sys-prefix --py rsconnect_jupyter
+export PATH=/opt/python/$PYTHON_VERSION/bin:$PATH
+/opt/python/$PYTHON_VERSION/bin/pip install jupyter jupyterlab rsp_jupyter rsconnect_jupyter rsconnect_python workbench_jupyterlab sklearn pandas
+/opt/python/$PYTHON_VERSION/bin/jupyter-nbextension install --sys-prefix --py rsp_jupyter 
+/opt/python/$PYTHON_VERSION/bin/jupyter-nbextension enable --sys-prefix --py rsp_jupyter 
+/opt/python/$PYTHON_VERSION/bin/jupyter-nbextension install --sys-prefix --py rsconnect_jupyter 
+/opt/python/$PYTHON_VERSION/bin/jupyter-nbextension enable --sys-prefix --py rsconnect_jupyter 
+/opt/python/$PYTHON_VERSION/bin/jupyter-serverextension enable --sys-prefix --py rsconnect_jupyter
