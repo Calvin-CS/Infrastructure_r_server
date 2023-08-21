1fac901d1a154c0f48df84e80d523d081d680db7
Chris Wieringa <cwieri39@calvin.edu>
2023-08-21 14:07:03 -0400

    updated URL again

--- install-Rstudio.sh
+++ install-Rstudio.sh
@@ -10,7 +10,6 @@ export RSW_NAME=rstudio-workbench
 export RSW_DOWNLOAD_URL=https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64
 
 #export RSW_VERSION_URL=`echo -n "${RSW_VERSION}" | sed 's/+/-/g'`
-echo "Downloading $RSW_VERSION_URL"
 #curl -O ${RSW_DOWNLOAD_URL}/${RSW_NAME}-${RSW_VERSION_URL}-amd64.deb
 curl -O ${RSW_DOWNLOAD_URL}/${RSW_NAME}-${RSW_VERSION}-amd64.deb
 gdebi -n ${RSW_NAME}-${RSW_VERSION_URL}-amd64.deb
