61e240106e868b8ed45702c08c89f9b563c7b235
Chris Wieringa <cwieri39@calvin.edu>
2023-08-21 14:18:59 -0400

    update URL again

--- install-Rstudio.sh
+++ install-Rstudio.sh
@@ -7,11 +7,11 @@
 #export RSW_VERSION=2022.12.0-353.pro20
 export RSW_VERSION=2023.06.1-524.pro1
 export RSW_NAME=rstudio-workbench
-export RSW_DOWNLOAD_URL=https://download2.rstudio.org/server/server/focal/amd64
+export RSW_DOWNLOAD_URL=https://download2.rstudio.org/server/focal/amd64
 
 #export RSW_VERSION_URL=`echo -n "${RSW_VERSION}" | sed 's/+/-/g'`
 #curl -O ${RSW_DOWNLOAD_URL}/${RSW_NAME}-${RSW_VERSION_URL}-amd64.deb
-echo "Downloading ${RSW_DOWNLOAD_URL}/${RSW_NAME}-${RSW_VERSION}-amd64.deb
+echo "Downloading ${RSW_DOWNLOAD_URL}/${RSW_NAME}-${RSW_VERSION}-amd64.deb"
 curl -O ${RSW_DOWNLOAD_URL}/${RSW_NAME}-${RSW_VERSION}-amd64.deb
 gdebi -n ${RSW_NAME}-${RSW_VERSION}-amd64.deb
 rm ${RSW_NAME}-${RSW_VERSION}-amd64.deb
